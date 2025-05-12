3a79f5ed552b7dd1f8d7ed1a1197879b1ee34044
Joan <j.ricartt@gmail.com>
2025-05-12 09:50:04 +0200

    mneu resources

--- app/Filament/Resources/MenuResource.php
+++ app/Filament/Resources/MenuResource.php
@@ -12,37 +12,60 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class MenuResource extends Resource
 {
     protected static ?string $model = Menu::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-book-open';
+
+    protected static ?string $navigationLabel = 'Cartes';
+    protected static ?string $label = 'Carta';
+    protected static ?string $pluralLabel = 'Cartes';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
-            ]);
+                TextInput::make('name')
+                    ->required()
+                    ->label('Nom de la carta'),
+
+                FileUpload::make('file_path')
+                    ->label('Arxiu PDF')
+                    ->directory('menus')
+                    ->acceptedFileTypes(['application/pdf'])
+                    ->downloadable()
+                    ->getUploadedFileNameForStorageUsing(function ($file, $livewire) {
+                        $name = Str::slug($livewire->data['name']);
+                        return "{$name}.pdf";
+                    })
+                    ->required(),
+            ])
+            ->columns(1);
     }
 
     public static function table(Table $table): Table
     {
         return $table
+            ->reorderable('order')
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('order')->label('Ordre'),
+                Tables\Columns\TextColumn::make('name')->label('Nom'),
+                Tables\Columns\TextColumn::make('file_path')
+                    ->label('Arxiu')
+                    ->url(fn(Menu $record) => Storage::url($record->file_path))
+                    ->openUrlInNewTab()
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
             ]);
     }
 
